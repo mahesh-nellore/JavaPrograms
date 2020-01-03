b0e2f65627ae5963ef93f10791acbbe9c28790bf
Nellore Mahesh <ce1094@ce-1094.cssad.compassitesinc.com>
2020-01-03 15:47:45 +0530

    Updated consecutiveOccurrencesCharacterInString program

--- src/ConsecutiveOccurrences.java
+++ src/ConsecutiveOccurrences.java
@@ -5,30 +5,24 @@ public class ConsecutiveOccurrences {
 
 	public static void main(String[] args) {
 		
-		String s = "aabbaaacbbbaaaa";
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		char[] ch = s.toCharArray();
-		for(int i = 0; i< ch.length; i++) {
-			int count = 1;
-			while(ch[i] == ch[i+1]) {
-				i++;
-				count++;
-				if(i+1 == ch.length)
+		String s = "aabbbbbbaaacbbbaaaa";
+		char ch = s.charAt(0);
+		int count = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			int temp = 1;
+			for (int j = i+1; j < s.length(); j++) {
+				if(s.charAt(i)!=s.charAt(j))
 					break;
-				
+				temp++;
 			}
-			if(!map.containsKey(ch[i]))
-				map.put(ch[i], count);
-			else {
-				if(map.get(ch[i]) < count) {
-					map.remove(ch[i]);
-					map.put(ch[i], count);
-				}
+			if(temp > count) {
+				count = temp;
+				ch = s.charAt(i);
 			}
 		}
 		
-		for(Map.Entry<Character, Integer>entry : map.entrySet())
-			System.out.println(entry.getKey()+":"+entry.getValue());
+		System.out.println("maximum occurence of a character ->"+ch+" : "+count);
 		
 
 	}
